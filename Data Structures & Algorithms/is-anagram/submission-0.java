8d7029dda170e4423dff44bcfa256d96699d6781
babasingh123 <40356645+babasingh123@users.noreply.github.com>
2026-06-03 08:54:00 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i <s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(char c : t.toCharArray()) {
+            if(!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+
+            map.put(c, map.get(c)-1);
+        }
+
+        return true;
+
+    }
+}
